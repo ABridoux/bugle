febc6045127fc70a6062a2c740425f63f085c635
Alexis Bridoux <alexis1bridoux@gmail.com>
2020-11-25 00:39:17 +0100

    Prepared for release

--- Sources/BugleCLT/BugleCommand.swift
+++ Sources/BugleCLT/BugleCommand.swift
@@ -3,13 +3,14 @@
 // Copyright (c) Alexis Bridoux 2020
 // MIT license, see LICENSE file for details
 
-import Foundation
+import Bugle
 import ArgumentParser
 
 struct BugleCommand: ParsableCommand {
 
     static let configuration = CommandConfiguration(commandName: "bugle",
                                                     abstract: "A tool to post and listen to distributed notifications",
+                                                    version: Version.current,
                                                     subcommands: [PostNotificationCommand.self,
                                                                   ListenCommand.self,
                                                                   AddCommand.self,
